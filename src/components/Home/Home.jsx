66564982600e99b1712dcd00dc623b17ce2282e3
Deepak Kumar Chouhan <dk397787@gmail.com>
2024-02-10 12:12:02 +0530

    remove some text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,21 +5,17 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className={styles.container}>
-       <div className={styles.main}>
-       <h2 className={styles.headingTitle}>Welcome to PopX
-       <Link to="/account">
-        Account
-      </Link>
-       </h2>
-      <p className={styles.mainDesc}>Lorem ipsum dolor, sit amet, </p>
-      <p className={styles.mainDesc}>consectetur adipisicing elit.</p>
-      <Link className={styles.signupBtn} to="/signup">
-        Create Account
-      </Link>
-      <Link className={styles.loginBtn} to="/login">
-        Already Registered? Login
-      </Link>
-       </div>
+      <div className={styles.main}>
+        <h2 className={styles.headingTitle}>Welcome to PopX</h2>
+        <p className={styles.mainDesc}>Lorem ipsum dolor, sit amet, </p>
+        <p className={styles.mainDesc}>consectetur adipisicing elit.</p>
+        <Link className={styles.signupBtn} to="/signup">
+          Create Account
+        </Link>
+        <Link className={styles.loginBtn} to="/login">
+          Already Registered? Login
+        </Link>
+      </div>
     </div>
   );
 };
